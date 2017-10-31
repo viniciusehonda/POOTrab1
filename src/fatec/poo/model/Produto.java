f45f5677558bcb579fc05741b10b0be8e00faf7a
honda <honda@192.168.1.33>
2017-10-31 21:55:06 -0200

    Classe Produto construida (completo)

--- src/fatec/poo/model/Produto.java
+++ src/fatec/poo/model/Produto.java
@@ -5,5 +5,48 @@ package fatec.poo.model;
  * @author honda
  */
 public class Produto {
+    int codigo;
+    String descricao;
+    int qtdeDisponivel;
+    double precoUnit;
+    int estoqueMin;
+    
+    public Produto(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public int getQtdeDisponivel() {
+        return qtdeDisponivel;
+    }
+
+    public void setQtdeDisponivel(int qtdeDisponivel) {
+        this.qtdeDisponivel = qtdeDisponivel;
+    }
+
+    public double getPrecoUnit() {
+        return precoUnit;
+    }
+
+    public void setPrecoUnit(double precoUnit) {
+        this.precoUnit = precoUnit;
+    }
+
+    public int getEstoqueMin() {
+        return estoqueMin;
+    }
+
+    public void setEstoqueMin(int estoqueMin) {
+        this.estoqueMin = estoqueMin;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
     
 }
