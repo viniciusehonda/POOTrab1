66de1527a5b31babd660fc806fcada2f53dd3712
honda <honda@192.168.1.33>
2017-10-31 21:54:35 -0200

    Classe ItemPedido construida (completo)

--- src/fatec/poo/model/ItemPedido.java
+++ src/fatec/poo/model/ItemPedido.java
@@ -5,5 +5,26 @@ package fatec.poo.model;
  * @author honda
  */
 public class ItemPedido {
+    int numeroItem;
+    int qtdeVendida;
+    
+    public ItemPedido(int numeroItem, int qtdeVendida) {
+        this.numeroItem = numeroItem;
+        this.qtdeVendida = qtdeVendida;
+    }
+
+    public int getQtdeVendida() {
+        return qtdeVendida;
+    }
+
+    public void setQtdeVendida(int qtdeVendida) {
+        this.qtdeVendida = qtdeVendida;
+    }
+
+    public int getNumeroItem() {
+        return numeroItem;
+    }
+    
+    
     
 }
