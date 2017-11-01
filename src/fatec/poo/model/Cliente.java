c84df499bc335a914a25f3be81d9e5942301c67d
0030481611018 <0030481611018@LTI6105.lti.fatecso.edu.br>
2017-11-01 11:12:36 -0200

    Implementação de: Ligação com a superclasse Pessoa Atributos e métodos da definição do diagrama Multiplicidade 1..* com a classe Pedido

--- src/fatec/poo/model/Cliente.java
+++ src/fatec/poo/model/Cliente.java
@@ -1,9 +1,33 @@
 package fatec.poo.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author honda
  */
-public class Cliente {
+public class Cliente extends Pessoa{
+    private double limiteCred;
+    private double limiteDisp;
     
+    private ArrayList<Pedido> pedidos;
+
+    public Cliente(String cpf, String nome, double limiteCred) {
+        super(cpf, nome);
+        this.limiteCred = limiteCred;
+        limiteDisp = limiteCred;
+    }
+    
+    public void addPedido(Pedido pedido){
+        pedidos.add(pedido);
+        pedido.setCliente(this);
+    }
+
+    public double getLimiteCred() {
+        return limiteCred;
+    }
+
+    public double getLimiteDisp() {
+        return limiteDisp;
+    }
 }
