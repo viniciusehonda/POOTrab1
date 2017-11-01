357c3ea837c70723f70108c867bb206ff995b7a9
0030481611018 <0030481611018@LTI6105.lti.fatecso.edu.br>
2017-11-01 11:16:49 -0200

    Implementação de: Ligação com a superclasse Pessoa Atributos e métodos da definição do diagrama Multiplicidade 1..* com a classe Pedido

--- src/fatec/poo/model/Vendedor.java
+++ src/fatec/poo/model/Vendedor.java
@@ -1,9 +1,40 @@
 package fatec.poo.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author honda
  */
-public class Vendedor {
+public class Vendedor extends Pessoa{
+    private double salarioBase;
+    private double comissao;
     
+    private ArrayList<Pedido> pedidos;
+
+    public Vendedor(String cpf, String nome, double salarioBase) {
+        super(cpf, nome);
+        this.salarioBase = salarioBase;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao / 100;
+    }
+    
+    public void addPedido(Pedido pedido){
+        pedidos.add(pedido);
+        pedido.setVendedor(this);
+    }
 }
