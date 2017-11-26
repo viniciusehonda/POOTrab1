65291e5a0359b5a349ed57cdcda6f9efe579363f
honda <honda@192.168.1.33>
2017-11-25 22:01:23 -0200

    DaoProduto 90% construido

--- src/fatec/poo/control/DaoProduto.java
+++ src/fatec/poo/control/DaoProduto.java
@@ -1,14 +1,87 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fatec.poo.control;
+
+import fatec.poo.model.Produto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author honda
  */
 public class DaoProduto {
+    private Connection conn;
     
+    public DaoProduto(Connection conn) {
+        this.conn = conn;
+    }
+    
+    public void inserir (Produto produto) {
+        PreparedStatement ps = null;
+        
+        try {
+            ps = conn.prepareStatement("INSERT into tbProduto("
+                    + "Cod_Prod, Desc_Prod, Qtd_Prod, Pco_Prod, MinEst_Prod) "
+                    + "VALUES(?,?,?,?,?");
+            ps.setInt(1, produto.getCodigo());
+            ps.setString(2, produto.getDescricao());
+            ps.setInt(3, produto.getQtdeDisponivel());
+            ps.setDouble(4, produto.getPrecoUnit());
+            ps.setInt(5, produto.getEstoqueMin());
+            
+            ps.execute();
+        } catch (SQLException ex){
+            System.out.println(ex.toString());
+        }
+    }
+    
+    public void alterar(Produto produto) {
+        
+        PreparedStatement ps = null;
+        
+        try {
+            ps = conn.prepareStatement("UPDATE tbProduto set Desc_Prod = ? "
+                    + "where Cod_Prod = ? ");
+            ps.setInt(1, produto.getCodigo());
+            ps.setString(2, produto.getDescricao());
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+    
+    public Produto consultar (int Codigo) {
+        Produto p = null;
+        
+        PreparedStatement ps = null;
+        
+        try {
+            ps = conn.prepareStatement("SELECT * from tbProduto where Cod_Prod = ?");
+            
+            ps.setInt(1, Codigo);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs.next() == true) {
+                p = new Produto(Codigo, rs.getString("Desc_Prod"));
+            }
+        } catch (SQLException ex){
+            System.out.println(ex.toString());
+        }
+        return (p);
+    }
+    
+    public void excluir (Produto produto) {
+        PreparedStatement ps = null;
+        
+        try {
+            ps = conn.prepareStatement("DELETE from tbProduto where Cod_Prod");
+            
+            ps.setInt(1, produto.getCodigo());
+            ps.execute();
+        } catch (SQLException ex){
+            System.out.println(ex.toString());
+        }
+    }
 }
